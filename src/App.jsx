2d94ee558aeec7b4d50272f56dfff65d55f6960a
saintlula <ehlinazdy@gmail.com>
2025-12-14 14:39:17 +1100

    Major background changes, terminal background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import MovingBlobs from "./components/movingBlobs";
-import "./styles/background.css"; // keep your current background
+import FaultyTerminal from "./components/FaultyTerminal";
 
 function App() {
   return (
-    <div>
-      <MovingBlobs />
-      {/* Other components and content go here */}
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      {/* Background */}
+      <FaultyTerminal
+        scale={2.5}
+        gridMul={[2, 1]}
+        digitSize={1.2}
+        timeScale={1}
+        scanlineIntensity={1}
+        glitchAmount={1}
+        flickerAmount={1}
+        noiseAmp={1}
+        chromaticAberration={0}
+        dither={0}
+        curvature={0.08}
+        tint="#A7EF9E"
+        mouseReact={true}
+        mouseStrength={0.5}
+        pageLoadAnimation={false}
+        brightness={1}
+      />
+
+      {/* Foreground content (add your site here later) */}
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          zIndex: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          pointerEvents: "none",
+        }}
+      >
+        {/* Example placeholder */}
+        {/* <h1>My Website</h1> */}
+      </div>
     </div>
   );
 }
